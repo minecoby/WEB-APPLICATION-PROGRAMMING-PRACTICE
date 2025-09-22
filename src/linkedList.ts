a165e1a82807b1c74897d386814623136c5d0ebe
minecoby <127065808+minecoby@users.noreply.github.com>
2025-09-22 11:45:02 +0900

    이중연결리스트 구현
    삭제, 양 끝 노드 검색, 역순출력, 노드검색 기능 구현

--- src/linkedList.ts
+++ src/linkedList.ts
@@ -1,19 +1,19 @@
 class Node<T>{
-    value: T;
-    prev: Node<T>;
-    next: Node<T>;
+  value: T;
+  prev: Node<T> | null = null;
+  next: Node<T> | null = null;
 
-    constructor(value: T){
-        this.value = value;
-    }
+  constructor(value: T){
+      this.value = value;
+  }
 }
 
 export class LinkedList<T>{
-    private head: Node<T> | null = null;
-    private tail: Node<T> | null = null;
-    private length: number = 0;
+  private head: Node<T> | null = null;
+  private tail: Node<T> | null = null;
+  private length: number = 0;
 
-    append(value: T): void {
+  append(value: T): void {
     const newNode = new Node(value);
     if (!this.head) {
       this.head = newNode;
@@ -28,8 +28,54 @@ export class LinkedList<T>{
     this.length++;
   }
 
+  delete(value: T): void{
+    let current = this.head;
+    while(current){
+      if(current.value == value){
+        if(current == this.head){
+          this.head = current.next;
+          if(!this.head) this.tail = null;
+          if(this.head) this.head.prev = null;
+        }
+        else if(current == this.tail){
+          this.tail = current.prev;
+          if(this.tail) this.tail.next = null;
+        }
+        else{
+        current.next.prev = current.prev;
+        current.prev.next = current.next;
+        }
+        this.length--;
+        break;
+      }
+      current= current.next
+    }
+  }
+
   size(): number {
     return this.length;
+  }
+
+  search(value: T): number | null{
+    let cnt:number = 1;
+    let current = this.head;
+    while(current){
+      if(current.value == value) return cnt;
+      cnt++;
+      current = current.next;
+    }
+    return null;
+  }
+
+  getFirst(): T|null{
+    let current = this.head;
+    if(current) return current.value;
+    else return null;
+  }
+  getLast(): T|null{
+    let current = this.tail;
+    if(current) return current.value;
+    else return null;
   }
 
   printList(): T[] {
@@ -38,6 +84,15 @@ export class LinkedList<T>{
     while (current) {
       result.push(current.value);
       current = current.next;
+    }
+    return result;
+  }
+  printListReverse(): T[] {
+    const result: T[] = [];
+    let current = this.tail;
+    while(current) {
+      result.push(current.value);
+      current = current.prev;
     }
     return result;
   }
